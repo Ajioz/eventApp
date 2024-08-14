3f201fa835eeb8a2ccae683f6f116bcde32c94df
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-08-14 11:38:07 +0100

    integrity check

--- pages/api/seeding.js
+++ pages/api/seeding.js
@@ -2,39 +2,53 @@
 import connectToDatabase from "../../dbConnect/db";
 import Event from "../../dbConnect/model";
 
-const DUMMY_EVENTS = [
-  {
-    title: "Programming for everyone",
-    description:
-      "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
-    location: "Somestreet 25, 12345 San Somewhereo",
-    date: "2021-05-12",
-    image: "images/coding-event.jpg",
-    isFeatured: false,
-  },
-  {
-    title: "Networking for introverts",
-    description:
-      "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
-    location: "New Wall Street 5, 98765 New Work",
-    date: "2021-05-30",
-    image: "images/introvert-event.jpg",
-    isFeatured: true,
-  },
-  {
-    title: "Networking for extroverts",
-    description:
-      "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
-    location: "My Street 12, 10115 Broke City",
-    date: "2022-04-10",
-    image: "images/extrovert-event.jpg",
-    isFeatured: true,
-  },
-];
 
-(async function () {
-  await connectToDatabase();
-  await Event.insertMany(DUMMY_EVENTS);
-  console.log("Dummy events inserted!");
-  process.exit(0);
-})();
+export default async function handler(req, res) {
+  try {
+    await connectToDatabase();
+    
+    // Check if the database is already seeded
+    const existingEvents = await Event.find({});
+    if (existingEvents.length > 0) {
+      return res.status(200).json({ message: "Database already seeded!" });
+    }
+
+    const DUMMY_EVENTS = [
+      {
+        title: "Programming for everyone",
+        description:
+          "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
+        location: "Somestreet 25, 12345 San Somewhereo",
+        date: "2021-05-12",
+        image: "images/coding-event.jpg",
+        isFeatured: false,
+      },
+      {
+        title: "Networking for introverts",
+        description:
+          "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
+        location: "New Wall Street 5, 98765 New Work",
+        date: "2021-05-30",
+        image: "images/introvert-event.jpg",
+        isFeatured: true,
+      },
+      {
+        title: "Networking for extroverts",
+        description:
+          "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
+        location: "My Street 12, 10115 Broke City",
+        date: "2022-04-10",
+        image: "images/extrovert-event.jpg",
+        isFeatured: true,
+      },
+    ];
+
+    // Clear existing data to avoid duplicates
+    await Event.deleteMany({});
+    await Event.insertMany(DUMMY_EVENTS);
+
+    res.status(200).json({ message: "Dummy events inserted!" });
+  } catch (error) {
+    res.status(500).json({ message: 'Seeding failed!', error });
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { getFeaturedEvents } from "../dummy-data";
 
 import EventList from "../components/events/eventList";
 
-function HomePage(){
+function HomePage() {
   const featuredEvent = getFeaturedEvents();
 
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function seedDatabase() {
+      const response = await fetch("/api/seed");
+      if (response.ok) {
+        console.log("Database seeded!");
+      } else {
+        console.error("Seeding failed.");
+      }
+    }
+
+    async function fetchEvents() {
+      const response = await fetch("/api/events");
+      const data = await response.json();
+      setEvents(data);
+    }
+
+    // Trigger seeding only once on first load
+    seedDatabase().then(() => fetchEvents());
+  }, []);
 
   return (
     <div>
       <EventList items={featuredEvent} />
     </div>
   );
-};
+}
 
 export default HomePage;
